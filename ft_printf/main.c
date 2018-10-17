28769de27e699db752de7a2b149a5ecbdeae8819
Andriy Gordiychuk <andriy@glenmax.com>
2018-10-17 21:24:37 +0300

    Initial Commit

---
+++ ft_printf/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ft_printf
+//
+//  Created by Andriy Gordiychuk on 17/10/2018.
+//  Copyright © 2018 Glenmax Ltd. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
