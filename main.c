24d8dfe178e0c0167185c80b709dc41b2db9c48b
Andriy Gordiychuk <andriy@glenmax.com>
2018-10-20 18:09:47 +0300

    Correct unicode length without locale

--- main.c
+++ main.c
@@ -5,10 +5,11 @@
 int main(void)
 {
     wchar_t a = 23532;
-
      //setlocale(LC_ALL, "");
-   printf("1:%03S\n", L"aà");
-   ft_printf("1:%03S\n", L"aà");
+   int i1 = printf("1:%03S\n", L"aà");
+   int i2 = ft_printf("2:%03S\n", L"aà");
+   printf("!1%d\n", i1);
+   printf("!2%d\n", i2);
    //printf("!%d\n", ft_printf("2%03S\n", L"à"));
    return 0;
 }
--- src/unicode.c
+++ src/unicode.c
@@ -13,7 +13,11 @@ void	print_2b_unicode(uint32_t c, t_format *flags, int *count)
 {
 	int	p;
 
-	print_left_padding(flags, "", 2);
+	if (MB_CUR_MAX >= 2)
+		*count += (2 >= flags->min_width) ? 2 : flags->min_width;
+	else
+		*count += (MB_CUR_MAX >= flags->min_width) ? MB_CUR_MAX : flags->min_width;
+	print_left_padding(flags, "", (MB_CUR_MAX >= 2) ? 2 : MB_CUR_MAX);
 	p = 0 | 0xC0;
 	p = p | ((c & 0x7C0) >> 6);
 	write(1, &p, 1);
@@ -22,20 +26,18 @@ void	print_2b_unicode(uint32_t c, t_format *flags, int *count)
 	p = 0 | 0x80;
 	p = p | (c & 0x3F);
 	write(1, &p, 1);
-	print_right_padding(flags, 2);
-	*count += (2 >= flags->min_width) ? 2 : flags->min_width;
+	print_right_padding(flags, (MB_CUR_MAX >= 2) ? 2 : MB_CUR_MAX);
 }
 
 void	print_3b_unicode(uint32_t c, t_format *flags, int *count)
 {
 	int p;
 
-	if (MB_CUR_MAX == 1)
-	{
-		print_1b_unicode(63, flags, count);
-		return ;
-	}
-	print_left_padding(flags, "", 3);
+	if (MB_CUR_MAX >= 3)
+		*count += (3 >= flags->min_width) ? 3 : flags->min_width;
+	else
+		*count += (MB_CUR_MAX >= flags->min_width) ? MB_CUR_MAX : flags->min_width;
+	print_left_padding(flags, "", (MB_CUR_MAX >= 3) ? 3 : MB_CUR_MAX);
 	p = 0 | 0xE0;
 	p = p | ((c & 0xF000) >> 12);
 	write(1, &p, 1);
@@ -49,20 +51,18 @@ void	print_3b_unicode(uint32_t c, t_format *flags, int *count)
 	p = 0 | 0x80;
 	p = p | (c & 0x3F);
 	write(1, &p, 1);
-	print_right_padding(flags, 3);
-	*count += (3 >= flags->min_width) ? 3 : flags->min_width;
+	print_right_padding(flags, (MB_CUR_MAX >= 3) ? 3 : MB_CUR_MAX);
 }
 
 void	print_4b_unicode(uint32_t c, t_format *flags, int *count)
 {
 	int p;
 
-	if (MB_CUR_MAX == 1)
-	{
-		print_1b_unicode(63, flags, count);
-		return ;
-	}
-	print_left_padding(flags, "", 4);
+	if (MB_CUR_MAX >= 4)
+		*count += (4 >= flags->min_width) ? 4 : flags->min_width;
+	else
+		*count += (MB_CUR_MAX >= flags->min_width) ? MB_CUR_MAX : flags->min_width;
+	print_left_padding(flags, "", (MB_CUR_MAX >= 4) ? 4 : MB_CUR_MAX);
 	p = 0 | 0x1E;
 	p = p | ((c & 0x1C0000) >> 18);
 	write(1, &p, 1);
@@ -81,6 +81,5 @@ void	print_4b_unicode(uint32_t c, t_format *flags, int *count)
 	p = 0 | 0x80;
 	p = p | (c & 0x3F);
 	write(1, &p, 1);
-	print_right_padding(flags, 4);
-	*count += (4 >= flags->min_width) ? 4 : flags->min_width;
+	print_right_padding(flags, (MB_CUR_MAX >= 4) ? 4 : MB_CUR_MAX);
 }
